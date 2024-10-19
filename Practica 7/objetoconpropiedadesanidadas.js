833694a6e285973051bd5d194dfb0f23fba2809c
Cristian RIvera <cr8539788@gmail.com>
2024-10-19 13:56:11 -0600

    Subiendo practica 7

---
+++ Practica 7/objetoconpropiedadesanidadas.js
@@ -0,0 +1,12 @@
+const estudiante = {
+    nombre: "Cris",
+    edad: 21,
+    direccion : {
+        calle: "Calle principal, Col Virgencita",
+        ciudad: "Santa Rosa de Lima"
+    },
+    materias : [ "matematicas" , "programacion" , "base de datos "]
+}
+
+console.log(estudiante.direccion.calle)
+console.log(estudiante.materias[2])
---
+++ Practica 7/objetoliteralbasico.js
@@ -0,0 +1,9 @@
+const persona = {
+    nombre : "Cris",
+    edad: 21,
+    profesion: "Ingeniero"
+};
+
+console.log(persona.nombre);
+console.log(persona.edad)
+console.log(persona.profesion)
